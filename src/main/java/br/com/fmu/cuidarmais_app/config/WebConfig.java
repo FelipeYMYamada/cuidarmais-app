a082ef207217dd82557343f89a1b6234e1b32af0
Felipe Mizuki Yamada <felipe.mizuki.yamada@gmail.com>
2024-11-20 19:32:51 -0300

    Config CORS

---
+++ src/main/java/br/com/fmu/cuidarmais_app/config/WebConfig.java
@@ -0,0 +1,23 @@
+package br.com.fmu.cuidarmais_app.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Value("${cors.originPatterns:default}")
+	private String corsOriginPatterns = "";
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+
+}
